020ca84d2311513c617c72e12e16f816c3c0f470
206563348 <wjcjenny@gmail.com>
2018-05-15 16:12:18 -0400

    update sencond buttons raphics

--- js/app.js
+++ js/app.js
@@ -1,40 +1,20 @@
 $(function() {
 	"use strict";
 	
-	var pymChild = new pym.Child();
-	
-	$('.item').hide();
-	$('#clicksBtnItem').show();
-	
-	$('.next').click(function(){
-		var thisId = $(this).attr('id');
-		$('.next').addClass('inactiveBtn');
-		$(this).removeClass('inactiveBtn');		
-		$('.item').hide();
-		$('#'+thisId+'Item').show();
-	});
-	
-	
-	$('.itemHead').click(function(){
-		$(this).next('.itemDetails').slideToggle(function() {
-			pymChild.sendHeight();
-		});	
-		
-		
-		if (! $(this).find('.arrowPng').hasClass('play')) {
-        	 $(this).find('.arrowPng').attr('src', 'img/arrow2.png');
-        	 $(this).find('.arrowPng').addClass('play');  
-    	} else  {
-        	 $(this).find('.arrowPng').attr('src', 'img/arrow.png');
-        	 $(this).find('.arrowPng').removeClass('play');
-    	}
-		//$('.arrowPng').attr('src','img/arrow2.png');
-	});
-	
-	pymChild.sendHeight();
+	$('.bttn').click(function() {
+		$( ".bttn" ).removeClass( 'active');
+		$( this ).addClass( 'active');
 
-$(window).resize(function(){ pymChild.sendHeight(); });
-	
+		if ($(this).attr('id') == "rect0") {
+			
+		}else if($(this).attr('id') == "rect1"){
+			
+		}else if($(this).attr('id') == "rect2"){
+			
+		}
+	});
+
+	$('#rect0').trigger("click");
 });
 
 
